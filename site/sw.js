3f39deba89c82d0849cc5b9bc051abbe726e82eb
MukaSanches <ssanches18@icloud.com>
2026-09-10 22:45:38 -0300

    Cache premium atelier layer offline

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
-const CACHE='permissionscope-shell-v6';
+const CACHE='permissionscope-shell-v7';
 const PAGES=['./index.html','./index.pt-BR.html','./index.es.html','./index.fr.html','./index.de.html','./index.ar.html','./index.ja.html','./index.zh-Hans.html'];
-const CORE=['./',...PAGES,'./404.html','./style.css','./premium.css','./device.css','./experience.css','./future.css','./responsive.css','./intelligence.css','./site.js','./experience.js','./future.js','./intelligence.js','./platform.js','./logo.svg','./manifest.webmanifest','./pwa-icon-192.svg','./pwa-icon-512.svg','./screenshots/en-US/access-light.png'];
+const CORE=['./',...PAGES,'./404.html','./style.css','./premium.css','./device.css','./experience.css','./future.css','./responsive.css','./intelligence.css','./atelier.css','./site.js','./experience.js','./future.js','./intelligence.js','./atelier.js','./platform.js','./logo.svg','./manifest.webmanifest','./pwa-icon-192.svg','./pwa-icon-512.svg','./screenshots/en-US/access-light.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
