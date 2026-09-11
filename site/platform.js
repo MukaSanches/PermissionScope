58828ec7534f821faad1c99233b96ece0e454a20
MukaSanches <ssanches18@icloud.com>
2026-09-10 22:04:31 -0300

    Restore resilient PWA platform bootstrap

---
+++ site/platform.js
@@ -0,0 +1,27 @@
+"use strict";
+(()=>{
+  if('serviceWorker' in navigator){
+    addEventListener('load',()=>navigator.serviceWorker.register('./sw.js',{scope:'./'}).catch(()=>{}),{once:true});
+  }
+
+  let installPrompt=null;
+  addEventListener('beforeinstallprompt',event=>{
+    event.preventDefault();
+    installPrompt=event;
+    document.documentElement.dataset.pwaInstallable='true';
+  });
+  addEventListener('appinstalled',()=>{
+    installPrompt=null;
+    delete document.documentElement.dataset.pwaInstallable;
+  });
+
+  const install=document.querySelector('[data-pwa-install]');
+  if(install){
+    install.addEventListener('click',async()=>{
+      if(!installPrompt)return;
+      await installPrompt.prompt();
+      installPrompt=null;
+      delete document.documentElement.dataset.pwaInstallable;
+    });
+  }
+})();
